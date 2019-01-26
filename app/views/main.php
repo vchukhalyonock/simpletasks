6f034c0f338d14865897b9173b1612b886b01531
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-26 22:07:22 +0200

    Introduce datatable

--- app/views/main.php
+++ app/views/main.php
@@ -7,22 +7,25 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/datatables.min.css"/>
 
     <title>Simple Tasks!</title>
 </head>
 <body>
 <div class="container">
     <h1>Simple tasks!</h1>
-    
-    <table class="table">
+
+    <table id="tasks-table" class="stripe" style="width:100%">
         <thead>
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Email</th>
-            <th scope="col">Name</th>
-            <th scope="col">Status</th>
-            <th scope="col">Task</th>
+            <th>#</th>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Task</th>
         </tr>
+        </thead>
+        <tbody>
         <?php foreach ($tasks as $key => $task):?>
             <tr>
                 <td><?php echo $task['id']?></td>
@@ -32,9 +35,6 @@
                 <td><?php echo $task['task']?></td>
             </tr>
         <?php endforeach;?>
-        </thead>
-        <tbody>
-
         </tbody>
     </table>
 </div>
@@ -44,5 +44,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/datatables.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
---
+++ www/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#tasks-table').DataTable({
+        "searching" : false
+    });
+} );
