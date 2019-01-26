9a9fc2dada376d0487912ff5e8148087b0c66d20
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-26 13:22:00 +0200

    Initial index.php

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+/**
+ * Set configuration using .env file
+ */
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+$dotenv->load();
