7bc73e166d0cc52c0d45aeeea1065dc4740f9817
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-27 01:11:44 +0200

    Add Admin controller with login logout possible

---
+++ app/contollers/Admin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers;
+
+use App\System\Classes\Controller;
+
+class Admin extends Controller {
+
+    private $_login = 'admin';
+    private $_password = '123';
+
+    public function __construct() {
+        session_start();
+        parent::__construct();
+    }
+
+
+    public function index() {
+        if($this->_isLoggedIn()) {
+            echo "Logged IN";
+        } else {
+            $this->render('admin_login');
+        }
+    }
+
+
+    public function login() {
+        if($this->_login == $this->request()->post('login') &&
+            $this->_password == $this->request()->post('password')) {
+            $_SESSION['loggedIn'] = true;
+        }
+
+        $this->response()->setHeader('Location', '/admin/');
+        $this->response()->send();
+    }
+
+    public function logout() {
+        session_destroy();
+        $this->response()->setHeader('Location', '/admin/');
+        $this->response()->send();
+    }
+
+
+    private function _isLoggedIn() {
+        return isset($_SESSION['loggedIn']) ? $_SESSION['loggedIn'] : false;
+    }
+}
---
+++ app/views/admin_login.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <title>Simple Tasks - Admin!</title>
+</head>
+<body>
+<div class="container">
+    <div class="col-5">
+    <h1>Admin Page</h1>
+    <form method="POST" action="/admin/login/" name="loginForm" id="login-form">
+        <div class="form-group">
+            <label for="login">Login:</label>
+            <input type="text" class="form-control" id="login" name="login" placeholder="Enter login">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+</div>
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
