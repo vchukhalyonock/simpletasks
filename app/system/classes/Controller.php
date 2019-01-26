cfa9baf69f5b5711bb67731065c2dcdc7101e30b
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-26 14:49:11 +0200

    Add request and respons as default to controller

--- app/system/classes/Controller.php
+++ app/system/classes/Controller.php
@@ -4,6 +4,34 @@ namespace App\System\Classes;
 
 use App\System\Interfaces\IController;
 
+/**
+ * Class Controller
+ * @package App\System\Classes
+ */
 abstract class Controller implements IController {
 
+    protected $_request;
+    protected $_response;
+
+    /**
+     * Controller constructor.
+     */
+    public function __construct() {
+        $this->_request = new Request();
+        $this->_response = new Response();
+    }
+
+    /**
+     * @return Request
+     */
+    protected function request() {
+        return $this->_request;
+    }
+
+    /**
+     * @return Response
+     */
+    protected function response() {
+        return $this->_response;
+    }
 }
